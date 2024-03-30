cd1e3ed61d528d52522717a5adef17a0edbda199
MacBook Pro <macbookpro@RinatSH.local>
2024-03-30 15:53:22 +0500

    update

---
+++ src/main/java/com/ShopNetwork/ShopNetwork/controllers/ItemController.java
@@ -0,0 +1,253 @@
+package com.ShopNetwork.ShopNetwork.controllers;
+
+import com.ShopNetwork.ShopNetwork.models.*;
+
+import com.ShopNetwork.ShopNetwork.repo.*;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.persistence.NoResultException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@SessionAttributes({"Cart", "verifedItems", "myFollowers"})
+public class ItemController {
+    @Autowired
+    private ItemRepository itemRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private RatingRepository ratingRepository;
+    @Autowired
+    private OrdersRepository ordersRepository;
+    @Autowired
+    private ResourceLoader resourceLoader;
+    @Autowired
+    private ItemChatRepo itemChatRepo;
+
+
+    //страница добавления нового товара
+    @GetMapping("/item/add")
+    public String add() {
+        return "add-item";
+    }
+
+
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/shopnetwork/target/classes/static/itemphoto";
+    @PostMapping("/item/add")
+    public String newItem(@RequestParam String title,
+                          @RequestParam String info,
+                          @RequestParam float price,
+                          @RequestParam("image") MultipartFile file,
+                          @RequestParam int count,
+                          @AuthenticationPrincipal UserDetails userDetails) throws IOException {
+        User user = userRepository.findByUsername(userDetails.getUsername());
+
+        StringBuilder fileNames = new StringBuilder();
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
+        fileNames.append(file.getOriginalFilename());
+        Files.write(fileNameAndPath, file.getBytes());
+        String image = "/itemphoto/" + file.getOriginalFilename();
+        Item item = new Item(title, info, image, price, user);
+        item.setCount(count);
+        itemRepository.save(item);
+        return "redirect:/item/" + item.getId();
+    }
+
+    @GetMapping("/itemphoto")
+    public String itemphoto() {
+        return "redirect:/index";
+    }
+
+
+    @GetMapping("/item/{id}")
+    public String show(@PathVariable(value = "id") long id, Model model, @AuthenticationPrincipal UserDetails userDetails) throws ChangeSetPersister.NotFoundException {
+
+        Item item = itemRepository.findById(id).orElseGet(Item::new);
+        User user = userRepository.findByUsername(item.getUser().getUsername());
+
+        System.out.println(item.getUser().getUsername());
+        //тут передаем товар
+        model.addAttribute("item", item);
+        //тут продавца
+        model.addAttribute("user", user);
+
+        //ну а тут отзывы
+        Iterable<Review> reviews = reviewRepository.findByItemId(id);
+        model.addAttribute("reviews", reviews);
+
+        //тут оценки
+        try {
+            model.addAttribute("rating", ratingRepository.getAverageRating(id));
+        } catch (NoResultException e) {
+            model.addAttribute("rating", e.toString());
+        }
+
+        if (userDetails != null) {
+            User authUser = userRepository.findByUsername(userDetails.getUsername());
+
+            //Проверка, куплен и доставлен ли товар, либо уже оценен, что бы отобразить или скрыть форму для рейтинга
+            if (ordersRepository.findOrderBuy(authUser, item, OrderStatus.DELIVERED) != null && ratingRepository.findMyRating(authUser, item) == null)
+                model.addAttribute("isHaveRating", true);
+             else
+                model.addAttribute("isHaveRating", false);
+
+
+            //Проверка, куплен и доставлен ли товар, либо уже оставлен отзыв, что бы отобразить или скрыть форму для отзывов
+            if (ordersRepository.findOrderBuy(authUser, item, OrderStatus.DELIVERED) != null && reviewRepository.findReview(authUser, item) == null)
+                model.addAttribute("isHaveReview", true);
+             else
+                model.addAttribute("isHaveReview", false);
+        }
+        return "show-item";
+    }
+
+    //удаление товара
+    @GetMapping("/item/{id}/delete")
+    public String deleteItem(@PathVariable (value = "id") long id, @AuthenticationPrincipal UserDetails userDetails) {
+        Item item = itemRepository.findById(id).orElse(new Item());
+        User user = userRepository.findByUsername(userDetails.getUsername());
+        Iterable<Orders> orders = ordersRepository.findByItemId(id);
+
+        List<Orders> orderIsWork = new ArrayList<>();
+        for (Orders o : orders) {
+            if (o.isWork())
+                orderIsWork.add(o);
+        }
+
+        //в конце проверим, в работе ли товар, а то может доставляется или еще что
+        if ((item.getUser().getId() == user.getId() ||
+                user.getRoles().contains(Role.ADMIN) ||
+                user.getRoles().contains(Role.MODERATOR)) && orderIsWork.isEmpty()) {
+
+                //раз уж удаляем товар, удаляем его во всех таблицах
+
+                for (Orders order : orders)
+                    ordersRepository.delete(order);
+
+                Iterable<Review> reviews = reviewRepository.findByItemId(id);
+                for (Review review : reviews)
+                    reviewRepository.delete(review);
+
+                Iterable<Rating> ratings = ratingRepository.findByItemId(id);
+                for (Rating rating : ratings)
+                    ratingRepository.delete(rating);
+
+                Iterable<ItemChat> itemChats = itemChatRepo.findByItemId(id);
+                for (ItemChat chat : itemChats)
+                    itemChatRepo.delete(chat);
+            itemRepository.delete(item);
+        }
+        return "redirect:/";
+    }
+
+    //страница редактирования товара
+    @GetMapping("/item/{id}/update")
+    public String update(@PathVariable(value = "id") long id, Model model,
+                         @AuthenticationPrincipal UserDetails userDetails) {
+        Item item = itemRepository.findById(id).orElse(new Item());
+        User user = userRepository.findByUsername(userDetails.getUsername());
+        model.addAttribute("item", item);
+        System.out.println(item.getUser().getUsername());
+        //редактировать мы можем товар если мы админ, модератор либо мы сами продавец
+        if ((item.getUser() == user) ||
+                user.getRoles().contains(Role.ADMIN) ||
+                user.getRoles().contains(Role.MODERATOR))
+            return "item-update";
+        else
+            return "redirect:/";
+    }
+
+    //делаем товар активным
+    @GetMapping("/item/{id}/setenabletrue")
+    public String setenabletrue(@PathVariable(value = "id") long id, Model model, @AuthenticationPrincipal UserDetails userDetails) {
+        Item item = itemRepository.findById(id).orElse(new Item());
+        User user = userRepository.findByUsername(userDetails.getUsername());
+        model.addAttribute("item", item);
+        System.out.println(item.getUser().getUsername());
+        //редактировать мы можем товар если мы сами продавец
+        if (item.getUser() == user) {
+            item.setEnabled(true);
+            itemRepository.save(item);
+            return "item-update";
+        }
+        else
+            return "redirect:/";
+    }
+
+    //делаем товар НЕ активным
+    @GetMapping("/item/{id}/setenablefalse")
+    public String setenablefalse(@PathVariable(value = "id") long id, Model model, @AuthenticationPrincipal UserDetails userDetails) {
+        Item item = itemRepository.findById(id).orElse(new Item());
+        User user = userRepository.findByUsername(userDetails.getUsername());
+        model.addAttribute("item", item);
+        //редактировать мы можем товар если мы сами продавец
+        if (item.getUser() == user) {
+            item.setEnabled(false);
+            itemRepository.save(item);
+            return "item-update";
+        }
+        else
+            return "redirect:/";
+    }
+
+
+
+    //редактирование товара
+    @PostMapping("/item/{id}/update")
+    public String updateItem(@PathVariable(value = "id") long id,
+                             @RequestParam String title,
+                             @RequestParam String info,
+                             @RequestParam("image") MultipartFile file,
+                             @RequestParam float price, @RequestParam int count,
+                             @AuthenticationPrincipal UserDetails userDetails) throws IOException {
+        Item item = itemRepository.findById(id).orElse(new Item());
+
+        StringBuilder fileNames = new StringBuilder();
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
+        fileNames.append(file.getOriginalFilename());
+        Files.write(fileNameAndPath, file.getBytes());
+        String image;
+        if (file.getOriginalFilename().isEmpty())
+            image = "/itemphoto/" + file.getOriginalFilename();
+        else
+            image = item.getImage();
+        //редактировать мы можем товар если мы сами продавец
+        if (item.getUser().getUsername().equals(userDetails.getUsername())) {
+            item.editItem(title, info, image, price);
+            item.setCount(count);
+            item.setVerifed(false);
+            itemRepository.save(item);
+            return "redirect:/item/" + id;
+        }
+        return "redirect:/item/" + id;
+    }
+
+    //поиск товара
+    @GetMapping("/search-item")
+    //как реализовано, нужно смотреть в файле ItemRepository
+    public String searchItem(@RequestParam(name = "search") String search, Model model) {
+        Iterable<Item> items = itemRepository.findItemByText(search, true);
+        model.addAttribute("items", items);
+        return "search";
+    }
+}
---
+++ src/main/java/com/ShopNetwork/ShopNetwork/models/Item.java
@@ -0,0 +1,59 @@
+package com.ShopNetwork.ShopNetwork.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title, info, image;
+    private int count; //количество товара
+
+    private float price;
+
+    private boolean verifed = false;
+    private boolean enabled;
+
+    @JoinColumn(name = "user_id")
+    @OneToOne(fetch = FetchType.EAGER)
+    private User user;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "item")
+    private Set<Review> review = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "item")
+    private Set<Orders> order = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "item")
+    private Set<Rating> ratings = new HashSet<>();
+
+    public Item(String title, String info, String image, float price, User user) {
+        this.title = title;
+        this.info = info;
+        this.image = image;
+        this.price = price;
+        this.user = user;
+        this.enabled = true;
+    }
+
+    public void editItem(String title, String info, String image, float price) {
+        this.title = title;
+        this.info = info;
+        this.image = image;
+        this.price = price;
+    }
+
+    public Item() {
+        this.enabled = true;
+    }
+}
